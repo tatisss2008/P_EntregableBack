f9deb851cff52b6e866ca4696d186bca6619dcc2
AnaM <test>
2023-07-15 22:59:50 -0500

    v inicial

---
+++ src/main/java/com/entregablebackend/entregablebackend/EntregablebackendApplication.java
@@ -0,0 +1,13 @@
+package com.entregablebackend.entregablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregablebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregablebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entregablebackend/entregablebackend/EntregablebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entregablebackend.entregablebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregablebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
